7412f3cd366ba77b3e046fb668e0050d4f19ca21
Ning Xiong <126361559+ningxiongnx@users.noreply.github.com>
2025-08-27 21:55:18 -0400

    add file

---
+++ claude-api/first_api_request.py
@@ -0,0 +1,39 @@
+# Load the environment variables 
+from dotenv import load_dotenv
+load_dotenv()
+
+# Create API client
+from anthropic import Anthropic
+client = Anthropic()
+model = "claude-sonnet-4-0"
+
+# Append message history to have multi-turn conversations 
+# Add user message to the messages list 
+def add_user_message(messages, content):
+    user_message = {"role":"user", "content": content}
+    messages.append(user_message)
+
+# Add assistant message to the messages list 
+def add_assistant_message(messages, content):
+    assistant_message = {"role":"assistant", "content": content}
+    messages.append(assistant_message)
+
+# Call API and return the response from assistant
+def chat(messages):
+    message = client.messages.create(
+        model=model,
+        max_tokens=1000,
+        messages=messages
+    )
+
+    #print(message)
+    response = message.content[0].text
+    print(response)
+    return response 
+
+messages = []
+add_user_message(messages, "Define quantum computing in one sentence")
+assistant_response = chat(messages)
+add_assistant_message(messages, assistant_response)
+add_user_message(messages, "Tell me more about it")
+assistant_response_2 = chat(messages)
